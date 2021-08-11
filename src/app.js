0df4b8d11fa7a2b4e17462b44a8c868e830d7ecd
prabhatk <prabhat.kasera@gmail.com>
2021-08-11 13:57:38 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { query } = require('express')
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -119,6 +120,6 @@ app.get('/weather', (req, res) => {
 // app.com/help
 // app.com/about
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port' + port)
 })
